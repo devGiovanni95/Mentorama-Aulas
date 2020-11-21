e60531312f0fe5e4e3cd695749a36f57eb067cba
Giovanni Almeida dos Santos <giovanni.DevJava@hotmail.com>
2020-11-20 21:45:22 -0300

    Quando usar Integer ou o Double

--- Classes-e-Objetos/src/um/Variavel.java
+++ Classes-e-Objetos/src/um/Variavel.java
@@ -2,6 +2,7 @@ package src.um;
 
 public class Variavel {
     public static void main(String[] args) {
+        /*
    /*     //  int valor1 = 12; //declarando de um tipo primitivo
       //  Integer valor1 = Integer.valueOf(12);
         Integer valor1 = 12;   //objeto outboxing  (objeto apparence)
@@ -17,8 +18,9 @@ public class Variavel {
 /*
 NullPointerException exemplo(quando seleciona um metodo de uma variavel que nao esta iniciada
  */
+        Integer valor1 = 25;
 
-        Integer valor1 = null;
+//        Integer valor1 = null;
         /*
         nunca podemos usar uma variavel com o valor null = sempre pede para que se declare uma numeração
         nestes casos pra nao ocorrer um erro utilizamos os tipos primitivos
---
+++ Classes-e-Objetos/src/um/Variavel1.java
@@ -0,0 +1,27 @@
+package src.um;
+
+public class Variavel1 {
+    public static void main(String[] args) {
+
+        /*
+        Mostra numero quebrados
+         */
+
+      /*  Double valor1 = 12d;
+        Double valor2 = 8d;
+
+        Double resultado = valor1 / valor2;
+
+        System.out.println(resultado);
+        */
+/*
+Mostra somente numeros inteiros
+ */
+        Integer valor1 = 12;
+        Integer valor2 = 8;
+
+        Integer resultado = valor1 / valor2;
+
+        System.out.println(resultado);
+    }
+}
